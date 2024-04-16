f335cfde414cae1e0e22d7bda4807cc25725152c
Gdebest69 <guyborovik@gmail.com>
2024-04-16 20:21:30 +0300

    Implemented the command

--- extensions/context_menus/nitro_react.py
+++ extensions/context_menus/nitro_react.py
@@ -1,4 +1,36 @@
+from discord.ui import Modal, TextInput
+from discord.utils import MISSING
 from main import *
+
+
+class EmojiModal(Modal):
+    emoji_name = TextInput(label="Emoji name", placeholder="The name of the emoji")
+    emoji_id = TextInput(label="Emoji id", placeholder="The id of the emoji")
+
+    def __init__(self, message: discord.Message) -> None:
+        super().__init__(title="Add the emoji data", timeout=None)
+        self.message = message
+
+    async def on_submit(self, interaction: discord.Interaction):
+        # data check
+        if not self.emoji_id.value.isdigit():
+            await interaction.response.send_message(
+                "The id must be a positive integer", ephemeral=True
+            )
+            return
+        emoji_str = f"{self.emoji_name}:{self.emoji_id}"
+        try:
+            await self.message._state.http.add_reaction(
+                self.message.channel.id, self.message.id, emoji_str
+            )
+            await interaction.response.defer()
+        except discord.errors.Forbidden:
+            await interaction.response.send_message(
+                "Sorry, but I can't react to the message due to lack of permissions in this server",
+                ephemeral=True,
+            )
+        except discord.errors.HTTPException:
+            await interaction.response.send_message("Unknown emoji", ephemeral=True)
 
 
 class NitroReactCommand(commands.Cog):
@@ -11,7 +43,8 @@ class NitroReactCommand(commands.Cog):
 
     async def nitro_react(
         self, interaction: discord.Interaction, message: discord.Message
-    ): ...
+    ):
+        await interaction.response.send_modal(EmojiModal(message))
 
 
 async def setup(bot: LoobiBot):
