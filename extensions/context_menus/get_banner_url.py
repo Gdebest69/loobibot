06c55fd31eb1e36e027f3fd8ea76905c6f4db988
Gdebest69 <guyborovik@gmail.com>
2024-06-14 18:04:15 +0300

    First implementation of get banner url command

---
+++ extensions/context_menus/get_banner_url.py
@@ -0,0 +1,30 @@
+from main import *
+
+
+class GetBannerUrlCommand(commands.Cog):
+    def __init__(self, bot: LoobiBot):
+        self.bot = bot
+        self.get_banner_url_ctx_menu = app_commands.ContextMenu(
+            name="Get banner URL",
+            callback=self.get_banner_url,
+        )
+        self.bot.tree.add_command(self.get_banner_url_ctx_menu)
+
+    @app_commands.user_install()
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def get_banner_url(
+        self, interaction: discord.Interaction, user: discord.User
+    ):
+        fetched_user = await self.bot.fetch_user(user.id)
+        if fetched_user.banner:
+            await interaction.response.send_message(
+                fetched_user.banner.url, ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                "This user doesn't have a custom banner", ephemeral=True
+            )
+
+
+async def setup(bot: LoobiBot):
+    await bot.add_cog(GetBannerUrlCommand(bot))
