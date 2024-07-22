448da358496e554ad7d339c55519d7773b3ce500
Gdebest69 <guyborovik@gmail.com>
2024-07-22 21:41:07 +0300

    Moddified a bit the join_roles code

--- extensions/listeners/join_roles.py
+++ extensions/listeners/join_roles.py
@@ -7,19 +7,15 @@ class JoinRoles(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        self.bot.logger.info(f"{member.name} left {member.guild.name}")
-
         # save roles
         roles_id_list = []
         for role in member.roles:
             roles_id_list.append(role.id)
-        roles_id_list.pop(0)  # the first role is always @everyone
+        roles_id_list.remove(member.guild.default_role.id)
         self.bot.get_guild_data(member.guild.id).roles[member.id] = roles_id_list
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        self.bot.logger.info(f"{member.name} joined {member.guild.name}")
-
         # add saved roles
         guild = member.guild
         guild_saved_roles = self.bot.get_guild_data(guild.id).roles
@@ -31,6 +27,7 @@ class JoinRoles(commands.Cog):
                         await member.add_roles(role, reason="Auto roles")
                     except discord.Forbidden:
                         pass
+            self.bot.logger.info(f"Added saved roles for {member} in {member.guild}")
 
 
 async def setup(bot: LoobiBot):
