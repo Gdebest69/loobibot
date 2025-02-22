bba46e9a5494aa136e33611087434f3ca379d91c
Gdebest69 <guyborovik@gmail.com>
2025-02-22 17:17:51 +0200

    Fix fun.py

--- extensions/fun.py
+++ extensions/fun.py
@@ -9,9 +9,9 @@ class FunStuff(commands.Cog):
         self.ok_message = "ok"
         self.yeshua_id = 746732379984494642
 
-    # Delete any message containing a link to pokemonshowdown.com from reac
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        # Delete any message containing a link to pokemonshowdown.com from reach
         if (
             message.guild is not None
             and message.guild.id == GUILD_ID
@@ -21,9 +21,7 @@ class FunStuff(commands.Cog):
             print_message(message, self.bot.logger)
             await message.delete()
 
-    # Delete any message in the ok channel that doesn't contain "ok"
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
+        # Delete any message in the ok channel that doesn't contain "ok"
         if (
             is_in_guild(message)
             and message.channel.id == self.ok_channel_id
@@ -36,9 +34,7 @@ class FunStuff(commands.Cog):
             except discord.Forbidden:
                 pass
 
-    # yeshuagay5
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
+        # yeshuagay5
         if plain_message(message.content).lower() == "gay":
             print_message(message, self.bot.logger)
             await message.reply(f"Just like {mention_user(self.yeshua_id)}")
