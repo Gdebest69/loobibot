1387b9afa2e713afdd4fd0442928e08fcc4b79e9
Gdebest69 <guyborovik@gmail.com>
2025-02-22 14:08:47 +0200

    Make a dedicated file for fun stuff

---
+++ extensions/fun.py
@@ -0,0 +1,38 @@
+from main import *
+
+
+class FunStuff(commands.Cog):
+    def __init__(self, bot: LoobiBot):
+        self.bot = bot
+        self.reach_id = 294452820353089536
+        self.ok_channel_id = 1264654642374115328
+        self.ok_message = "ok"
+
+    # Delete any message containing a link to pokemonshowdown.com from reac
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if (
+            message.guild is not None
+            and message.guild.id == GUILD_ID
+            and "pokemonshowdown.com" in message.content
+            and message.author.id == self.reach_id
+        ):
+            await message.delete()
+
+    # Delete any message in the ok channel that doesn't contain "ok"
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if (
+            is_in_guild(message)
+            and message.channel.id == self.ok_channel_id
+            and plain_message(message.content).lower() != self.ok_message
+        ):
+            try:
+                await message.delete()
+                await message.author.send("Not ok")
+            except discord.Forbidden:
+                pass
+
+
+async def setup(bot: LoobiBot):
+    await bot.add_cog(FunStuff(bot))
