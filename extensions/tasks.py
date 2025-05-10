a3e28fdd680240d5d7569aaf661c326d71a0a413
Gdebest69 <guyborovik@gmail.com>
2025-05-10 14:25:13 +0300

    Remove rgb feature

--- extensions/tasks.py
+++ extensions/tasks.py
@@ -6,23 +6,12 @@ from main import *
 class Tasks(commands.Cog):
     def __init__(self, bot: LoobiBot):
         self.bot = bot
-        self.current_color = len(RGB_COLORS) - 1
         if not bot.testing:
             self.auto_save_data.start()
-            self.rgb_effect.start()
 
     @tasks.loop(seconds=SAVE_TIME_SEC, reconnect=True)
     async def auto_save_data(self):
         self.bot.save_data()
-
-    @tasks.loop(seconds=RGB_EFFECT_COOL_DOWN_SEC, reconnect=True)
-    async def rgb_effect(self):
-        if self.current_color >= len(RGB_COLORS) - 1:
-            self.current_color = 0
-        else:
-            self.current_color += 1
-        role = self.bot.get_guild(GUILD_ID).get_role(RGB_ROLE_ID)
-        await role.edit(color=discord.Color(RGB_COLORS[self.current_color]))
 
 
 async def setup(bot: LoobiBot):
