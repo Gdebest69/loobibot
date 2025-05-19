a59b62506814ddec63df3a935319c621374caaa1
Gdebest69 <guyborovik@gmail.com>
2025-05-19 09:54:02 +0300

    Add embed support for get plain text ctx menu command

--- extensions/context_menus/get_plain_text.py
+++ extensions/context_menus/get_plain_text.py
@@ -1,5 +1,27 @@
+import json
 from io import StringIO
 from main import *
+
+
+class LastEmbedView(discord.ui.View):
+    def __init__(self, last_embed: discord.Embed):
+        super().__init__(timeout=TIMEOUT)
+        self.embed = last_embed
+        self.message: discord.Message = None
+
+    @discord.ui.button(label="View last embed", style=discord.ButtonStyle.green)
+    async def send_last_embed(self, interaction: discord.Interaction, button):
+        await interaction.response.send_message(
+            file=discord.File(
+                StringIO(json.dumps(self.embed.to_dict(), indent=4)),
+                filename="embed9.json",
+            ),
+            ephemeral=True,
+        )
+
+    async def on_timeout(self):
+        if self.message is not None:
+            await self.message.edit(view=None)
 
 
 class GetPlainTextCommand(commands.Cog):
@@ -16,15 +38,32 @@ class GetPlainTextCommand(commands.Cog):
     async def get_plain_text(
         self, interaction: discord.Interaction, message: discord.Message
     ):
-        if len(message.content) == 0:
+        embed_files = [
+            discord.File(
+                StringIO(json.dumps(embed.to_dict(), indent=4)),
+                filename=f"embed{i}.json",
+            )
+            for i, embed in enumerate(message.embeds)
+        ]
+        files = (
+            [discord.File(StringIO(message.content), filename="message.txt")]
+            if len(message.content) > 0
+            else []
+        ) + embed_files
+        if len(files) == 0:
             await interaction.response.send_message(
                 "Message content is empty", ephemeral=True
             )
+        elif len(files) <= 10:
+            await interaction.response.send_message(files=files, ephemeral=True)
         else:
+            view = LastEmbedView(message.embeds[9])
             await interaction.response.send_message(
-                file=discord.File(StringIO(message.content), filename="message.txt"),
+                files=files[:10],
                 ephemeral=True,
+                view=view,
             )
+            view.message = await interaction.original_response()
 
 
 async def setup(bot: LoobiBot):
