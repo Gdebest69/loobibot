8efee455bc956add801d105a376294889f4b85c3
Gdebest69 <guyborovik@gmail.com>
2025-07-03 20:00:04 +0300

    Show avatar decoration in get avatar url command

--- extensions/context_menus/get_avatar_url.py
+++ extensions/context_menus/get_avatar_url.py
@@ -4,6 +4,7 @@ from main import *
 class GetAvatarUrlCommand(commands.Cog):
     def __init__(self, bot: LoobiBot):
         self.bot = bot
+        self.avatar_decoration_to_gif_prefix_url = "https://ezgif.com/apng-to-gif?url="
         self.get_avatar_url_ctx_menu = app_commands.ContextMenu(
             name="Get avatar URL",
             callback=self.get_avatar_url,
@@ -19,6 +20,12 @@ class GetAvatarUrlCommand(commands.Cog):
         user_avatar = user.avatar if user.avatar is not None else user.default_avatar
         if user_avatar != user.display_avatar:
             message += "\n" + user_avatar.url
+        if user.avatar_decoration is not None:
+            message += (
+                "\n"
+                + user.avatar_decoration.url
+                + f"\nConvert avatar decoration to gif: {self.avatar_decoration_to_gif_prefix_url}{user.avatar_decoration.url}"
+            )
         await interaction.response.send_message(message, ephemeral=True)
 
 
