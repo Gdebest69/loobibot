aed307ca479ed96f614d4b3ca5520544d60371ac
Gdebest69 <guyborovik@gmail.com>
2025-08-02 14:23:03 +0300

    Remove unused bat files

--- create_library.bat
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/Rapptz/discord.py.git
-cd discord.py
-git remote add fork https://github.com/Puncher1/discord.py.git
-git fetch origin
-git fetch fork
-git checkout voice-status-rest
--- update_library.bat
+++
@@ -1,6 +0,0 @@
-cd discord.py
-git checkout master
-git fetch --all
-git pull
-git checkout voice-status-rest
-git merge master
